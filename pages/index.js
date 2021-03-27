1f59e96769c1404f9cba575af567bbeccfcb7385
Carlos Silva Jr <[carlus.jr@hotmail.com]>
2021-03-27 20:13:16 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Psicólogos na InterNet</h1>
+        </div>
+    )
+    
+}
+
+export default Home

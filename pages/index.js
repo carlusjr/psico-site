6484b99bb42e1673d3f006d0dfd00eb9463c7b41
Carlos Silva Jr <[carlus.jr@hotmail.com]>
2021-04-01 22:06:26 -0300

    contadores

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,43 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import Header from './Header'
+import Link from 'next/link'
 
-function Home() {
-    return (
-        <div>
-            <h1>Psicólogos na InterNet</h1>
-            <p></p><br></br>
-            <h2>Página em construção...</h2>
-        </div>
-    )
-    
+function App() {
+
+  let teste=10;
+
+  function addMeuContador(){
+    teste++;
+  }
+
+  return (
+    <div>
+      <Header title="PSICÓLOGOS NA InterNet" meuContador={teste}/>
+      <input type="button" value="Add Contador Carlos" onClick={addMeuContador} />
+      <p></p><br></br>
+      <h2>Página em construção...</h2>
+      <Contador />
+      <p></p><br></br>
+      <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+      </Link>
+    </div>
+  );
 }
 
-export default Home
+function Contador() {
+  const [contador, setcontador] = useState(0);
+
+  function addContador() {
+    setcontador(contador + 1)
+  }
+
+  return (
+    <div>
+      <h2>Contador Felipe: {contador} </h2>
+      <input type="button" value="Add Contador Felipe" onClick={addContador} />
+    </div>
+  )
+}
+
+export default App;
---
+++ pages/sobre.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+function Sobre() {
+  return (
+    <div>
+      <h1>SOBRE</h1>
+      <p></p><br></br>
+      <Link href="/">
+        <a>Acessar página Home</a>
+      </Link>  
+    </div>
+  );
+
+}
+
+export default Sobre;

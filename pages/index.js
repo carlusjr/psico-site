cbf07d6013ff84bf6cb0640d92c859961f09dfd1
Carlos Silva Jr <[carlus.jr@hotmail.com]>
2021-04-04 16:39:00 -0300

    menu no index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-//import { useState } from 'react'
-import Menu from './Menu.js'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 import Head from 'next/head'
 
 function App() {
@@ -19,18 +19,53 @@ function App() {
   );
 }
 
-// function Contador() {
-//   const [contador, setcontador] = useState(0);
+function Menu() {
+  const [count, setCount] = useState(0);
+  const [activeItem, setActive] = useState('home');
+  
+  function mudaActive(e) {
+    setActive(e.target.id);
+  }
 
-//   function addContador() {
-//     setcontador(contador + 1)
-//   }
-//   return (
-//     <div>
-//       <h2>Contador Felipe: {contador} </h2>
-//       <input type="button" value="Add Contador Felipe" onClick={addContador} />
-//     </div>
-//   )
-// }
+  function addCount() {
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    let x = document.getElementById("menuTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+
+    if (activeItem) {
+      console.log('Atualiza menu', count);
+      let itensMenu = document.getElementsByName('menuItem');
+      let qtdItens = itensMenu.length;
+      for (let i=0; i < qtdItens; i++){
+        itensMenu[i].className = "";
+      }
+      let menuItem = document.getElementById(activeItem);
+      menuItem.className = "active";
+    }
+
+  });
+
+  return (
+    <div className="topnav" id="menuTopnav">
+      <a href="#home" name="menuItem" id="home" onClick={mudaActive}>Home</a>
+      <a href="#psicologos" name="menuItem" id="psicologos" onClick={mudaActive}>Psicólogos</a>
+      <a href="#procedimentos" name="menuItem" id="procedimentos" onClick={mudaActive}>Procedimentos</a>
+      <a href="#linksUteis" name="menuItem" id="linksUteis" onClick={mudaActive}>Links Úteis</a>
+      <a href="#faleConosco" name="menuItem" id="faleConosco" onClick={mudaActive}>Fale conosco</a>
+      <Link href="/sobre" id="sobre"><a name="menuItem" onClick={mudaActive}>Sobre</a></Link>
+      <a href="#teste" className="icon" onClick={addCount}>
+        <i className="fa fa-bars"></i>
+      </a>
+    </div>
+  )
+}
+
 
 export default App;

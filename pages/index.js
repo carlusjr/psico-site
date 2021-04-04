b2291d8a9860a0b3d5cd319e41474cab9d82e5eb
Carlos Silva Jr <[carlus.jr@hotmail.com]>
2021-04-04 02:57:55 -0300

    Menu nav-bar

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,36 @@
-import { useState } from 'react'
-import Header from './Header'
-import Link from 'next/link'
+//import { useState } from 'react'
+import Menu from './Menu.js'
+import Head from 'next/head'
 
 function App() {
-
-  let teste=10;
-
-  function addMeuContador(){
-    teste++;
-  }
-
-  return (
+  return (    
     <div>
-      <Header title="PSICÓLOGOS NA InterNet" meuContador={teste}/>
-      <input type="button" value="Add Contador Carlos" onClick={addMeuContador} />
-      <p></p><br></br>
-      <h2>Página em construção...</h2>
-      <Contador />
-      <p></p><br></br>
-      <Link href="/sobre">
-        <a>Acessar página Sobre</a>
-      </Link>
+      <Head>
+        <title>PsicoNET</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+      </Head>      
+      <section>
+        <h1>PSICÓLOGOS na Internet</h1>
+        <Menu />
+        <h2>Página em construção...</h2>
+      </section>
     </div>
   );
 }
 
-function Contador() {
-  const [contador, setcontador] = useState(0);
+// function Contador() {
+//   const [contador, setcontador] = useState(0);
 
-  function addContador() {
-    setcontador(contador + 1)
-  }
-
-  return (
-    <div>
-      <h2>Contador Felipe: {contador} </h2>
-      <input type="button" value="Add Contador Felipe" onClick={addContador} />
-    </div>
-  )
-}
+//   function addContador() {
+//     setcontador(contador + 1)
+//   }
+//   return (
+//     <div>
+//       <h2>Contador Felipe: {contador} </h2>
+//       <input type="button" value="Add Contador Felipe" onClick={addContador} />
+//     </div>
+//   )
+// }
 
 export default App;

75476f3b6745a07f59b09972f649795ccbca40bc
Carlos Silva Jr <[carlus.jr@hotmail.com]>
2021-04-24 21:49:46 -0300

    API desativada

--- pages/api/artigo/[art_nome].js
+++ pages/api/artigo/[art_nome].js
@@ -1,11 +1,11 @@
-import { getArtigo } from '../../../src/db'
+//import { getArtigo } from '../../../src/db'
 
 export default async function (req, res) {
   const art_nome = req.query.art_nome;
-  const artigo = await getArtigo(art_nome);
-  if (artigo) {
-    res.json( artigo );
+  //const artigo = await getArtigo(art_nome);
+  if (art_nome) {
+    res.json( { message: `${art_nome} não está disponível!` } );
   } else {
-    res.status(400).json( { message: 'artigo inexistente' } );
+    res.status(400).json( { message: 'Request de API inválido!' } );
   }  
 }

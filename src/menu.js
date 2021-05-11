4d60153491b21fbad5bdaf6f7d207a229cd093a2
Carlos Silva Jr <[carlus.jr@hotmail.com]>
2021-05-11 11:25:35 -0300

    nav menu

--- src/menu.js
+++ src/menu.js
@@ -17,16 +17,16 @@ export default function Menu(props) {
   });
 
   return (
-    <div className="topnav" id="menuTopnav">
-      {props.menuItens.map(item =><Link href={`/paginas/${item.menu_nome}`} key={item.menu_nome}>
-          <a className={(item.menu_nome == props.paginaAtiva ? "active" : "")}>{item.menu_titulo}</a>
-        </Link>)
+    <nav className="topnav" id="menuTopnav">
+      {props.menuItens.map(item => <Link href={`/paginas/${item.menu_nome}`} key={item.menu_nome}>
+        <a className={(item.menu_nome == props.paginaAtiva ? "active" : "")}>{item.menu_titulo}</a>
+      </Link>)
       };
       <a href="#teste"
         className="icon"
         onClick={() => setSendRequest(true)}>
         <i className="material-icons">menu</i>
       </a>
-    </div>
+    </nav>
   )
 }

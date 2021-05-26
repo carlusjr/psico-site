c50818917b2d0fa997839f0b5754755ac1e7dd3d
carlusjr <crsilvajr@gmail.com>
2021-05-26 19:23:12 -0300

    auth sem console log

--- src/auth.js
+++ src/auth.js
@@ -3,9 +3,6 @@ import { verify } from 'jsonwebtoken'
 export default function IsAuth(request) {
   const mySecret = process.env.UUID_JWT;
   const token = request.headers.authorization
-  const decode = verify(token, mySecret);
-  console.log('Secret: ', mySecret);
-  console.log('Token:', token);
-  console.log('Decode:', decode);
+  const decode = verify(token, mySecret);  
   return true;
 }

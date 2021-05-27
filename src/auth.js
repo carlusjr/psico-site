f77228b4fb37396c9009ae24381b5a40f8edf290
carlusjr <crsilvajr@gmail.com>
2021-05-27 11:36:43 -0300

    removido auth.js

--- src/auth.js
+++
@@ -1,8 +0,0 @@
-import { verify } from 'jsonwebtoken'
-
-export default function IsAuth(request) {
-  const mySecret = process.env.UUID_JWT;
-  const token = request.headers.authorization
-  const decode = verify(token, mySecret);  
-  return true;
-}

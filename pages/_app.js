1c3efe2040187a280ae5bde980c0898e0724ce33
carlusjr <crsilvajr@gmail.com>
2021-07-22 19:58:30 -0300

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import TagManager from 'react-gtm-module';
+import { useEffect } from 'react';
 import { AuthProvider } from '../src/contexts/AuthContext'
 import { ToastContextProvider } from "../src/contexts/toastContext"
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'G-C912LDSL0F' });
+  }, []);
+
   return (
     <AuthProvider>
       <ToastContextProvider>
--- src/components/Head.js
+++ src/components/Head.js
@@ -8,7 +8,8 @@ export default function Header({ title }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <title>{title}</title>
+        <link rel="icon" type="image/png" href="favicon.png"/>
+        <title>{title}</title>        
       </Head>      
     </>
   )
